174699b3cdfba095dcca53d106e533c7b633f47b
ashnard <guizalsc@outlook.com>
2023-12-16 20:41:03 -0800

    fix framerate

--- html/overlay/avatars/boxboxer/avatar.js
+++ html/overlay/avatars/boxboxer/avatar.js
@@ -5,12 +5,12 @@ const metadata = {
 		idle: {
 			left: [5, 6],
 			right: [0, 1],
-			frameRate: 20,
+			frameRate: 2,
 		},
 		walking: {
 			left: [7, 8, 9],
 			right: [2, 3, 4],
-			frameRate: 20,
+			frameRate: 6,
 		},
 	},
 	frameHeight: 32,
--- html/overlay/avatars/kirbypelo/avatar.js
+++ html/overlay/avatars/kirbypelo/avatar.js
@@ -5,12 +5,12 @@ const metadata = {
 		idle: {
 			left: [3, 4, 5],
 			right: [0, 1, 2],
-			frameRate: 20,
+			frameRate: 3,
 		},
 		walking: {
 			left: [14, 13, 12, 11],
 			right: [6, 7, 8, 9],
-			frameRate: 15,
+			frameRate: 8,
 		},
 	},
 	frameHeight: 29,
