249d63b5f70dbd1b0416790b04ee6e3594a0dee4
ashnard <guizalsc@outlook.com>
2023-12-16 20:16:38 -0800

    add box boxer

---
+++ html/overlay/avatars/boxboxer/avatar.js
@@ -0,0 +1,25 @@
+import Avatar from "../avatar.js";
+
+const metadata = {
+	animations: {
+		idle: {
+			left: [5, 6],
+			right: [0, 1],
+			frameRate: 4,
+		},
+		walking: {
+			left: [7, 8, 9],
+			right: [2, 3, 4],
+			frameRate: 6,
+		},
+	},
+	frameHeight: 32,
+	frameWidth: 32,
+	scale: 3,
+};
+
+class ExtendedAvatar extends Avatar {
+	//
+}
+
+export { metadata, ExtendedAvatar };
