541f399ee8daebde397b4c9e7d48df68ba0e6acc
ashnard <guizalsc@outlook.com>
2023-12-16 20:22:40 -0800

    boxer frames

--- html/overlay/avatars/boxboxer/avatar.js
+++ html/overlay/avatars/boxboxer/avatar.js
@@ -5,12 +5,12 @@ const metadata = {
 		idle: {
 			left: [5, 6],
 			right: [0, 1],
-			frameRate: 4,
+			frameRate: 6,
 		},
 		walking: {
 			left: [7, 8, 9],
 			right: [2, 3, 4],
-			frameRate: 6,
+			frameRate: 10,
 		},
 	},
 	frameHeight: 32,
